a1f36718f2643765a1bb62c9b1b95f81dbba375d
BiagioliCassandra <biagioli.cassandra@gmail.com>
2022-10-19 10:23:13 +0200

    correction du lancement de flèches de la baliste

--- Assets/_Scripts/Traps/ShootArrow.cs
+++ Assets/_Scripts/Traps/ShootArrow.cs
@@ -1,16 +1,18 @@
 using Photon.Pun;
 using System.Collections;
 using UnityEngine;
+using Photon.Realtime;
 
 namespace _Scripts.TrapSystem.Datas
 {
     public class ShootArrow : MonoBehaviourPunCallbacks
     {
         [SerializeField] private Transform _arrowSpawnPosition;
-
         [SerializeField] private GameObject _arrowPrefab;
+
         private float _arrowSpeed = 20f;
         private float _timeLeft = 1f;
+        private GameObject _arrow;
 
         void Update()
         {
@@ -24,15 +26,21 @@ namespace _Scripts.TrapSystem.Datas
 
         IEnumerator Shoot()
         {
-/*            if (!PViewIsMine)
-                return;*/
+            foreach (Player player in PhotonNetwork.PlayerList)
+            {
+                if(player.IsLocal)
+                {
+                    _arrow = PhotonNetwork.Instantiate(_arrowPrefab.name, _arrowSpawnPosition.position, _arrowSpawnPosition.rotation);
+                    _arrow.GetComponent<Rigidbody>().velocity = _arrowSpawnPosition.forward * _arrowSpeed;
 
-            GameObject _arrow = PhotonNetwork.Instantiate(_arrowPrefab.name, _arrowSpawnPosition.position, _arrowSpawnPosition.rotation);
-            _arrow.GetComponent<Rigidbody>().velocity = _arrowSpawnPosition.forward * _arrowSpeed;
-
-            yield return new WaitForSeconds(3f);
-
-            Destroy(_arrow);
+                    yield return new WaitForSeconds(3f);
+                    Destroy(_arrow);
+                }
+                else
+                {
+                    yield break;
+                }
+            }
             //TODO : Une fois le d�cors mis en place, destroy la fl�che si elle touche un �lement du d�cors (murs, d�co...)
         }
     }
