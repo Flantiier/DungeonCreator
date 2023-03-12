66bb442973edc4d5576f6cdda4b36da8f60ee01c
Florian <florianlernould@gmail.com>
2023-03-12 16:43:04 +0100

    Rename Objects Tool

--- Assets/Editor/RenameObjects.cs
+++ Assets/Editor/RenameObjects.cs
@@ -9,18 +9,34 @@ namespace _Scripts.Editor
     {
         [SerializeField] private string prefix;
         [SerializeField] private string suffix;
-        [SerializeField] private int removeFromStart = 0;
-        [SerializeField] private int removeFromEnd = 0;
+        [SerializeField, Min(0)] private int removeFromStart = 0;
+        [SerializeField, Min(0)] private int removeFromEnd = 0;
 
+        [Button("Rename select(ion", ButtonSizes.Medium)]
         private void Rename()
         {
+            if (Selection.objects.Length <= 0)
+                return;
+
             for (int i = 0; i < Selection.objects.Length; i++)
             {
                 //Get the string
-                string name = Selection.objects[i].name;
-                name = prefix + name + suffix;
-                
+                Object obj = Selection.objects[i];
+                string name = obj.name;
+                name = removeFromStart <= 0 ? name : name.Remove(0, removeFromStart);
+                name = removeFromEnd <= 0 ? name : name.Remove(name.Length - removeFromEnd);
+                obj.name = prefix + name + suffix;
             }
+
+            ResetValue();
+        }
+
+        private void ResetValue()
+        {
+            prefix = string.Empty;
+            suffix = string.Empty;
+            removeFromStart = 0;
+            removeFromEnd = 0;
         }
 
         [MenuItem("Tools/Rename Objects")]
