d46ed79f0fbc5e23a6a48a4e12f099eb8b349b36
Florian Lernould <florianlernould7@gmail.com>
2023-04-07 12:16:04 +0200

    Création prefabs des ennemis

--- Assets/Editor/CustomEditorWindow.cs
+++ Assets/Editor/CustomEditorWindow.cs
@@ -21,7 +21,8 @@ namespace Assets.Editor
             tree.AddAssetAtPath("Game Launcher", "Assets/Editor/SO/GameLauncherTool.asset", typeof(GameLauncher));
             tree.AddAssetAtPath("Game Management", "Assets/_ScriptablesObjects/GameManagement/GameProperties.asset", typeof(GameProperties));
             tree.AddAssetAtPath("Folder Navigator", "Assets/Editor/SO/Folder Paths.asset");
-            tree.AddAllAssetsAtPath("Edit Traps", "Assets/_ScriptablesObjects/Traps", typeof(TrapSO));
+            tree.AddAllAssetsAtPath("Edit Traps/Traps", "Assets/_ScriptablesObjects/Traps", typeof(TrapSO));
+            tree.AddAllAssetsAtPath("Edit Traps/Ennemies", "Assets/_ScriptablesObjects/Traps/Traps Enemies", typeof(TrapSO));
             tree.AddAllAssetsAtPath("Edit Characters", "Assets/_ScriptablesObjects/Characters/Edit Characters");
 
             return tree;
