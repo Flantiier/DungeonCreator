dfc5430dde1924133b35c6769238114f52a17e8d
Florian Lernould <florianlernould7@gmail.com>
2023-06-07 11:29:26 +0200

    Aventuriers visibles à tarevrs murs + mana à 10

--- Assets/_Scripts/UI/GamePlay/SliderElement.cs
+++ Assets/_Scripts/UI/GamePlay/SliderElement.cs
@@ -5,23 +5,29 @@ namespace _Scripts.UI.Gameplay
 {
 	public class SliderElement : MonoBehaviour
 	{
-		[SerializeField] private Slider slider;
-		[SerializeField] private FloatVariable variable;
+        #region Variables
+		[SerializeField] protected FloatVariable variable;
+        [SerializeField] protected Slider slider;
+        #endregion
 
-		private void Start()
+        #region Builts_In
+        protected virtual void Start()
 		{
 			slider.maxValue = variable.value;
 			SetSliderValue();
 		}
 
-		private void Update()
+		protected virtual void Update()
 		{
 			SetSliderValue();
 		}
+        #endregion
 
-		private void SetSliderValue()
+        #region Methods
+        protected virtual void SetSliderValue()
 		{
 			slider.value = variable.value;
 		}
-	}
+        #endregion
+    }
 }
